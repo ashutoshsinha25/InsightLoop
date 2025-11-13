515543df4b6b011a6463f50a5da02961112653c3
Ashutosh <ashutoshsinha519@gmail.com>
2025-11-13 20:26:43 +0530

    add skeleton code.

--- README.md
+++ README.md
@@ -5,4 +5,5 @@ AI-Powered Feedback &amp; Insight Engine for Conversations
 
 # Work Log
 5th Nov: Started working on HLD document for InsightLoop project.
+
 6th Nov: Continuning with HLD, focusing on data layers and processing pipeline.
--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from insightloop!")
+from fastapi import FastAPI 
+from app.api.router import api_router 
+from app.core.config import settings 
 
 
-if __name__ == "__main__":
-    main()
+def create_app() -> FastAPI:
+    app = FastAPI(
+        title = 'InsightLoop',
+        version="0.0.1",
+        description="Backend API for InsightAPI GenAI Conversational Insight Engine."
+    )
+
+    app.include_router(api_router, prefix="/api/v1")
+    return app 
+
+
+app = create_app()
